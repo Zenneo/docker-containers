586740dd3fa91105d4586d29e4997027c3eea1c6
Thomas Schmitz <agrar.pfurtz@gmail.com>
2016-08-12 18:24:21 +0000

    added forgotten sh

---
+++ gmod-vanilla/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# update Gmod server
+/opt/steamcmd/steamcmd.sh +login anonymous +force_install_dir /opt/garrysmod +app_update 4020 validate +quit
+
+# run Gmod server
+/opt/garrysmod/srcds_run -steamdir /opt/steamcmd -port ${GMOD_PORT} "$@"
