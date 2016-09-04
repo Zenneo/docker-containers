ff8b5069c4c2c8f17e154aea57fe9a1309bd883f
Thomas Schmitz <agrar.pfurtz@gmail.com>
2016-09-04 11:00:38 +0000

    another steamclient fix attempt using LD_LIBRARYPATH

--- tower-unite/docker-entrypoint.sh
+++ tower-unite/docker-entrypoint.sh
@@ -4,9 +4,8 @@
 /opt/steamcmd/steamcmd.sh +login anonymous +force_install_dir /opt/tu +app_update 439660 validate +quit
 
 # fix missing steamlib
-cp /opt/steamcmd/linux32/* /opt/tu/.steam/sdk32/
 cp /opt/steamcmd/linux32/steamclient.so /opt/tu/Tower/Binaries/Linux/
-cp /opt/steamcmd/linux64/steamclient.so /opt/tu/Tower/Binaries/Linux/
+LD_LIBRARY_PATH=/usr/lib64:${LD_LIBRARY_PATH}
 
 # run TU server
 /opt/tu/Tower/Binaries/Linux/TowerServer-Linux-Shipping -log  -Port="$TU_PORT" -QueryPort="$QUERY_PORT" "$@"
