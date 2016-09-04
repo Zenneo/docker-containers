308d6897f4689af8cf46b0681c1e2ae17673e878
Thomas Schmitz <agrar.pfurtz@gmail.com>
2016-09-04 00:10:10 +0000

    fixed port issue

--- tower-unite/docker-entrypoint.sh
+++ tower-unite/docker-entrypoint.sh
@@ -4,4 +4,4 @@
 /opt/steamcmd/steamcmd.sh +login anonymous +force_install_dir /opt/tu +app_update 439660 validate +quit
 
 # run TU server
-/opt/tu/Tower/Binaries/Linux/TowerServer-Linux-Shipping -log  -Port="$TU_PORT" -QueryPort="$TU_PORT" "$@"
+/opt/tu/Tower/Binaries/Linux/TowerServer-Linux-Shipping -log  -Port="$TU_PORT" -QueryPort="$QUERY_PORT" "$@"
