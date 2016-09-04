fbfec83aa8d45a6a1b74e5683cdb5ef72bbcdd8b
Thomas Schmitz <agrar.pfurtz@gmail.com>
2016-09-04 09:46:32 +0000

    fixed steamcmd issue

--- tower-unite/docker-entrypoint.sh
+++ tower-unite/docker-entrypoint.sh
@@ -4,7 +4,7 @@
 /opt/steamcmd/steamcmd.sh +login anonymous +force_install_dir /opt/tu +app_update 439660 validate +quit
 
 # fix missing steamlib
-cp /opt/steamcmd/linux32/steamclient.so /opt/tu/.steam/sdk32/steamclient.so
+cp /opt/steamcmd/linux32/* /opt/tu/.steam/sdk32/
 
 # run TU server
 /opt/tu/Tower/Binaries/Linux/TowerServer-Linux-Shipping -log  -Port="$TU_PORT" -QueryPort="$QUERY_PORT" "$@"
