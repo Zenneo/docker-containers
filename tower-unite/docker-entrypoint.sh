d5f2483648ca90d8398810785c770243573b066d
Thomas Schmitz <agrar.pfurtz@gmail.com>
2016-09-17 13:05:30 +0000

    another dependencie fix attempt for tu

--- tower-unite/docker-entrypoint.sh
+++ tower-unite/docker-entrypoint.sh
@@ -7,11 +7,5 @@
 cp /opt/steamcmd/linux32/* /opt/tu/Tower/Binaries/Linux/
 export LD_LIBRARY_PATH=/usr/lib32:${LD_LIBRARY_PATH}
 
-apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y \
-    mono-gmcs mono-xbuild mono-dmcs libmono-corlib4.0-cil libmono-system-data-datasetextensions4.0-cil \
-    libmono-system-web-extensions4.0-cil libmono-system-management4.0-cil libmono-system-xml-linq4.0-cil \
-    dos2unix clang xdg-user-dirs
-
 # run TU server
 /opt/tu/Tower/Binaries/Linux/TowerServer-Linux-Shipping -log  -Port="$TU_PORT" -QueryPort="$QUERY_PORT" "$@"
