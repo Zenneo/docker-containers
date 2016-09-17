881f100673020bdea346f470d45591b3cb60cfbd
Thomas Schmitz <agrar.pfurtz@gmail.com>
2016-09-17 11:42:08 +0000

    another TU lib fix attempt

--- tower-unite/docker-entrypoint.sh
+++ tower-unite/docker-entrypoint.sh
@@ -4,10 +4,7 @@
 /opt/steamcmd/steamcmd.sh +login anonymous +force_install_dir /opt/tu +app_update 439660 validate +quit
 
 # fix missing steamlib
-mkdir /opt/Steam/sdk32/
-ln -s /opt/steamcmd/linux32/steamclient.so /opt/Steam/sdk32/steamclient.so
 cp /opt/steamcmd/linux32/* /opt/tu/Tower/Binaries/Linux/
-cp /opt/steamcmd/linux64/* /opt/tu/Tower/Binaries/Linux/
 export LD_LIBRARY_PATH=/usr/lib32:${LD_LIBRARY_PATH}
 
 # run TU server
