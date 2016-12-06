672928195edf50a36ca6d6b471dd2d2de5c49259
Thomas Schmitz <Zennoe@users.noreply.github.com>
2016-12-06 09:17:58 +0100

    fixed missing steamclient.so

--- tf2/docker-entrypoint.sh
+++ tf2/docker-entrypoint.sh
@@ -4,8 +4,8 @@
 /opt/steamcmd/steamcmd.sh +login anonymous +force_install_dir /opt/tf2 +app_update 232250 validate +quit
 
 # Link linux32 to .steam
-mkdir ~/.steam/
-ln -s /opt/steamcmd/linux32 ~/.steam/sdk32
+mkdir -p ~/.steam/sdk32
+cp /opt/steamcmd/linux32/* ~/.steam/sdk32/
 
 # run TF2 server
 /opt/tf2/srcds_run -steamdir /opt/steamcmd -port "$TF2_PORT" "$@"
