c24030b8c4f17e44d24fdfd142fd73a13d92873e
Thomas Schmitz <agrar.pfurtz@gmail.com>
2017-02-28 11:05:02 +0000

    added autoupdater to tu

--- tower-unite/docker-entrypoint.sh
+++ tower-unite/docker-entrypoint.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-# update TU server
-/opt/steamcmd/steamcmd.sh +login anonymous +force_install_dir /opt/tu +app_update 439660 validate +quit
-
 # fix missing steamlib
 cp /opt/tu/linux64/steamclient.so /opt/tu/Tower/Binaries/Linux/
-#export LD_LIBRARY_PATH=/usr/lib32:${LD_LIBRARY_PATH}
 
-# run TU server
-exec /opt/tu/Tower/Binaries/Linux/TowerServer-Linux-Shipping -log  -Port="$TU_PORT" -QueryPort="$QUERY_PORT" "$@"
+# called intially no matter what
+function update_and_start {
+    # update TU server
+    /opt/steamcmd/steamcmd.sh +login anonymous +force_install_dir /opt/tu +app_update 439660 validate +quit
+    # start TU server
+    exec /opt/tu/Tower/Binaries/Linux/TowerServer-Linux-Shipping -log  -Port="$TU_PORT" -QueryPort="$QUERY_PORT" "$@"
+}
+
+# vars
+buildid_new=0
+buildid_old=0
+
+# if no autoupdate: update once and run TU server
+if [AUTOUPDATE = false]; then
+    update_and_start
+else
+    while [1]; do
+        # save buildid
+        buildid_new=$(curl -s 'https://steampics-mckay.rhcloud.com/info?apps=439660&prettyprint=1' | jq '.apps."439660".depots.branches.public.buildid' | sed 's/"//g')
+    
+        # if buildid_new != buildid_old: stop server, update and restart
+        if [$buildid_old != $buildid_new]; then
+            killall TowerServer-Linux-Shipping
+            buildid_old = buildid_new
+            update_and_start
+        fi
+        
+        # sleep
+        sleep AUTOINTERVAL
+    done
+        
+fi
